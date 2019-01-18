a8b8db9e132da9d64343e44f413f6380f5716ce3
Zaida Escila Martínez Moreno <zaida.martinezmoreno@mail.mcgill.ca>
2019-01-17 21:51:32 -0500

    Add files via upload
    Open the file with a text editor. Write down the source path (folder where your mnc.gz files are) and your destination folder (final path).
    Then, in the terminal, write ~Path_to_the_file~/mnc2nii_conversion.ssh

---
+++ mnc2nii_conversion.sh
@@ -0,0 +1,30 @@
+# Conversion of MNC.GZ files to NII files
+
+#!/bin/bash
+
+#Input the location of the source folder and the final folder
+
+SourcePath=/$WRITE$HERE$THE$PATH$WHERE$MNC.GZ$FILES$ARE
+
+FinalPath=/$WRITE$HERE$YOUR$DESTINATION$FOLDER$PATH
+
+list=`ls $SourcePath`
+
+for name in $list
+do 
+    gzip -dk $SourcePath$name
+done
+
+echo Unzipping done
+
+list2=`ls $SourcePath*.mnc`
+
+for name2 in $list2
+do
+    mnc2nii $name2 $name2.nii
+    newname=`cut -d'.' -f1 <<< $name2.ncm.nc`
+    mv $name2.nii $newname.nii
+    mv $newname.nii $FinalPath
+done
+
+echo All files are ready in the specified folder.
